c093fe8e8faf990108ed21b8d6a278077b9e027d
wilnermena <wi_an9@hotmail.com>
2023-09-03 20:30:44 -0400

    Mi tdd 2

--- src/validador.test.js
+++ src/validador.test.js
@@ -1,9 +1,15 @@
 import validarcampo from "./validar.js";
 
 describe("VALIDAR EL CAMPO", () => {
+
   it("El post deberia tener un Título", () => {
     const dato='TITULO DEL POST';
     expect(validarcampo(dato)).toEqual(true);
+  });
+
+  it("El post deberia tener un Título", () => {
+    const dato='';
+    expect(validarcampo(dato)).toEqual(false);
   });
 
 });
