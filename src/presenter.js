440647de6d9f4669b055a6371a07b6a8880b94e3
wilnermena <wi_an9@hotmail.com>
2023-09-03 20:19:56 -0400

    Mi primer comit

---
+++ src/presenter.js
@@ -0,0 +1,19 @@
+import sumar from "./sumador";
+
+const titulo = document.querySelector("#titulo");
+const contenido = document.querySelector("#detalle");
+const boton = document.querySelector("#publicar");
+const div = document.querySelector("#resultado-div");
+
+boton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const titulopost=titulo.value;
+  const contenidopost=contenido.value;
+
+  //div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML=`<p>TITULO:${titulopost}</p>
+  <p>CONTENIDO:${contenidopost}</p>
+  
+  `;
+});
